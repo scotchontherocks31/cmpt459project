57dea551016b265ba7fc7c84a6c86bc015ad4d8f
zapsel <julianjo2@gmail.com>
2021-02-18 02:24:57 -0800

    added age formatting and column removal

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import os
+import re
 import math
 
 o = os.getcwd()
@@ -12,12 +13,12 @@ def clean_database(data):
 
 	return data
 
-def gaussian_remove_outlers(data, column):
+def gaussian_remove_outliers(data, column):
 	mean = data[column].mean()
 	std = data[column].std()
 
 	cutoff = std * 3
-	lower_bound, upper_bound = mean + std, mean - std
+	lower_bound, upper_bound = mean + cutoff, mean - cutoff
 	for index, row in data:
 		if row[column] > upper_bound or row[column] <= lowerbound:
 			data.drop(index, axis=0, inplace=True)
@@ -26,7 +27,36 @@ def gaussian_remove_outlers(data, column):
 def handle_outliers(data):
 	return data
 
+def clean(data):
+	#remove unnecessary columns
+	data.drop(['source', 'additional_information'], axis=1, inplace=True)
+
+	#remove rows containing NaN values with minimal loss to data
+	data.dropna(subset['longitude', 'latitude', 'province', 'date_confirmation'], inplace=True)
+
+	#standardize age formatting
+	for index, row in data.iterrows():
+		if row['age'] != np.NaN:
+			m = re.match("(\d{1,})-(\d{1,})", str(row['age']))
+			if m:
+				data.loc[index,'age'] = round(sum(map(int, m.groups()))/2)
+				n = re.match("(\d{1,})\W",str(row['age']))
+			if n:
+				data.loc[index,'age'] = m.groups()[0]
+
+	return data	
+
+
 def main():
+	print("\n\nTeam Losers: Milestone 1\n\n")
+	print("Performing Data Cleaning and Handling of NaN Values...")
+
+	print("Cleaning Train Data...")
+	cleaned_train = clean(train_data)
+
+	print("Cleaning Test Data...")
+	cleaned_test = clea(test_data)
+
 
 	return
 
