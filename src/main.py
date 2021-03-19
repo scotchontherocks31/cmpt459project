e79be508c92fdda78e7a62854ea6929a790cfa59
zapsel <julianjo2@gmail.com>
2021-03-19 01:30:01 -0700

    improved preprocessing for classifier compatibility
    - changed categorical data to numerical
    - dropped redundant location columns
    - dropped date columns with date ranges

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ import math
 import pandas as pd 
 
 o = os.getcwd()
-
-data = pd.read_csv(o + "\\..\\data\\cases_train_processed.csv", parse_dates = True)
+global data 
 model_path = o + "\\..\\models\\"
 
 '''
@@ -17,15 +16,38 @@ model_path = o + "\\..\\models\\"
 data = pd.read_csv(o + "/../data/cases_train_processed.csv", parse_dates = True)
 model_path = o + "/../models/"
 '''
+# converts string Series into a cataegorized int series1 for data analysis
+def categorize_column(data):
+	i = 0
+	for value in data.unique():
+		data.replace(value, i, inplace = True)
+		i += 1
+	data = data.apply(pd.to_numeric)
+	return data
+
 def build_model(data):
 
 	#Model code here!!
 
-	# save model to model_path + "model_name.pkl"
+	#save model to model_path + "model_name.pkl"
 	return
 
 def main():
 	print("\n\nTeam Losers: Milestone 2\n\n")
+	print("Modifying data for classifiers...\n")
+
+	data = pd.read_csv(o + "\\..\\data\\cases_train_processed.csv", parse_dates = True)
+	data['sex'] = categorize_column(data['sex'])
+	data['outcome'] = categorize_column(data['outcome'])
+	data['Combined_Key'] = categorize_column(data['Combined_Key'])
+
+	#handling date column
+	#removes columns where date_confirmation has a daterange
+	data = data.loc[~data['date_confirmation'].str.contains('-'), :]  
+
+	#might need to drop date column if classifiers can't handle it
+	data = data.drop(columns=['province', 'country'])
+
 	print("Splitting data into test and validation sets...\n")
 
 	train, val = train_test_split(data, test_size=0.2, random_state=69, shuffle=True)
@@ -34,6 +56,8 @@ def main():
 	build_model(train)
 
 	print("Model building completed, Evaluating models...")
+
+
 	#-------- Functions for latter parts of the milestone-------------------------
 	#evaluate(model,train,val)
 
