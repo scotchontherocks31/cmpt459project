1c14d14292fa55a11a9fbe8b39b5d2081786dacb
iabdulme <iabdulme@sfu.ca>
2021-03-22 12:19:26 -0700

    knn model update

--- src/main.py
+++ src/main.py
@@ -7,20 +7,25 @@ import pandas as pd
 import pickle
 
 # Model-specific imports
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
 
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
 
 o = os.getcwd()
 global data 
+'''
 model_path = o + "\\..\\models\\"
-adapath = model_path + 'adaModel.pkl'
-
 '''
-#For Mac/Linux
-data = pd.read_csv(o + "/../data/cases_train_processed.csv", parse_dates = True)
 model_path = o + "/../models/"
-'''
+knnpath = model_path + 'knnModel.pkl'
+
+
+#For Mac/Linux
+#data = pd.read_csv(o + "/../data/cases_train_processed.csv", parse_dates = True)
+#model_path = o + "/../models/"
+
 # converts string Series into a cataegorized int series1 for data analysis
 def categorize_column(data):
 	i = 0
@@ -32,9 +37,18 @@ def categorize_column(data):
 
 def build_model(data):
 
+	X = data.drop(columns=['outcome'])
+	y = data['outcome']
 
+	knn_model = KNeighborsClassifier(n_neighbors=10)
+
+	model2 = knn_model.fit(X,y)
+
+	list_pickle = open(knnpath, 'wb')
+	pickle.dump(model2, list_pickle)
+	list_pickle.close()
 	#save model to model_path + "model_name.pkl"
-	print("First model saved to folder...\n")
+	print("Second model saved to folder...\n")
 
 	return
 
@@ -48,7 +62,7 @@ def main():
 	print("\n\nTeam Losers: Milestone 2\n\n")
 	print("Modifying data for classifiers...\n")
 
-	data = pd.read_csv(o + "\\..\\data\\cases_train_processed.csv", parse_dates = True)
+	data = pd.read_csv(o + "/../data/cases_train_processed.csv", parse_dates = True)
 	data['sex'] = categorize_column(data['sex'])
 	data['outcome'] = categorize_column(data['outcome'])
 	data['Combined_Key'] = categorize_column(data['Combined_Key'])
@@ -65,14 +79,14 @@ def main():
 
 	train, val = train_test_split(data, test_size=0.2, random_state=69, shuffle=True)
 
-	print("Building first model...\n")	
+	print("Building second model...\n")	
 	build_model(train)
 
 	print("Model building completed, Evaluating models...")
 
 
 	#-------- Functions for latter parts of the milestone-------------------------
-	#evaluate(model,train,val)
+	#evaluate(train,val)
 
 	#show_overfit(model)
 
