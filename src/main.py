342678db05e972189bbf0f8bca2ad60a0ed24501
snooh <snooh@sfu.ca>
2021-03-22 19:12:30 -0700

    XGBoost complete, evaluate function complete

--- src/main.py
+++ src/main.py
@@ -2,81 +2,189 @@ from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 import os
 import math
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import pickle
+import xgboost as xgb
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support as score, precision_score, recall_score, f1_score
+import shap
 
 # Model-specific imports
 
-#Import scikit-learn metrics module for accuracy calculation
+# Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
 
 o = os.getcwd()
-global data 
-model_path = o + "\\..\\models\\"
-adapath = model_path + 'adaModel.pkl'
+global data
+# model_path = o + "\\..\\models\\"
 
-'''
-#For Mac/Linux
-data = pd.read_csv(o + "/../data/cases_train_processed.csv", parse_dates = True)
+# For Mac/Linux
+data = pd.read_csv(o + "/../data/cases_train_processed.csv", parse_dates=True)
 model_path = o + "/../models/"
-'''
+
+adapath = model_path + 'adaModel.pkl'
+xgpath = model_path + 'xgbModel.pkl'
+
+ada_pass = 'ada'
+xg_pass = 'xg'
+knn_pass = 'knn'
+
 # converts string Series into a cataegorized int series1 for data analysis
+
+
 def categorize_column(data):
-	i = 0
-	for value in tqdm(data.unique()):
-		data.replace(value, i, inplace = True)
-		i += 1
-	data = data.apply(pd.to_numeric)
-	return data
-
-def build_model(data):
+    i = 0
+    for value in tqdm(data.unique()):
+        data.replace(value, i, inplace=True)
+        i += 1
+    data = data.apply(pd.to_numeric)
+    return data
 
 
-	#save model to model_path + "model_name.pkl"
-	print("First model saved to folder...\n")
+def build_model(train, val):
 
-	return
+    print('XGBoost Model:\n')
 
-def evaluate(train, val):
-	
-	#importing models
-	#AdaBoost
-	return
+    print('1\tSetting training variables')
+    dtrain = train.drop(columns='outcome')
+    dval = val.drop(columns='outcome')
+    dt_label = train['outcome']
+    dv_label = val['outcome']
+
+    print('2\tSetting custom XGB DMatrices...')
+    xg_dtrain = xgb.DMatrix(dtrain, label=dt_label)
+    xg_dval = xgb.DMatrix(dval, label=dv_label)
+    xg_test = xgb.DMatrix(dtrain, label=dt_label)
+
+    print('3\tSetting parameters...')
+    param = {
+        'objective': 'multi:softmax',
+        'max_depth': 6,
+        'eta': 0.1,
+        'subsample': 0.5,
+        'colsample_bytree': 0.5,
+        'num_parallel_tree': 1,
+        'min_child_weight': 1000,
+        'gamma': 100,
+        'num_class': 4
+    }
+    evallist = [(xg_dval, 'eval'), (xg_dtrain, 'train')]
+    num_round = 500
+    early_stopping_rounds = 3
+
+    # XGB training GO!
+    print('***\tXGB Initiate\t***')
+    xgb_model = xgb.train(param, xg_test, num_round, evallist,
+                          early_stopping_rounds=early_stopping_rounds)
+    print('***\tCOMPLETE\t***')
+
+    print('4\tExporting model')
+    pickle.dump(xgb_model, open(xgpath, "wb"))
+    return
+
+
+def evaluate(train, val, filepath, str):
+
+    # importing models
+    print('*****\tEVALUATION INITIATE\t*****')
+    model = pickle.load(open(filepath, "rb"))
+
+    dtrain = train.drop(columns='outcome')
+    dval = val.drop(columns='outcome')
+    dt_label = train['outcome']
+    dv_label = val['outcome']
+
+    train_prediction = [round(value) for value in dt_label]  # from training
+    prediction = [round(value) for value in dv_label]  # from validation
+
+    print('1\tAccuracy measure:\n')
+    model_predict = None
+    if(str == xg_pass):
+        print('Setting up XGBoost vars & model prediction...')
+        xg_dtrain = xgb.DMatrix(dtrain, label=dt_label)
+        xg_dval = xgb.DMatrix(dval, label=dv_label)
+
+        model_predict = model.predict(xg_dval)
+        train_accuracy = accuracy_score(
+            model.predict(xg_dtrain), train_prediction)
+        print("Train Accuracy: %.2f%%" % (train_accuracy * 100.0))
+        accuracy = accuracy_score(model_predict, prediction)
+        print("Validation Accuracy: %.2f%%" % (accuracy * 100.0))
+
+    print('2\tConfusion Matrix:\n')
+    cm_labels = train['outcome'].unique().tolist()
+    cm = confusion_matrix(dv_label, model_predict, cm_labels)
+    print(cm)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(cm)
+    plt.title('Confusion matrix')
+    fig.colorbar(cax)
+    ax.set_xticklabels([''] + cm_labels)
+    ax.set_yticklabels([''] + cm_labels)
+    plt.xlabel('Predicted Values')
+    plt.ylabel('Actual Values')
+    plt.show()
+
+    print('3\tPrecision, Recall & F-Score:\n')
+    precision = precision_score(dv_label, model_predict, average="macro")
+    print('Precision: %f' % precision)
+    # recall: tp / (tp + fn)
+    recall = recall_score(dv_label, model_predict, average="macro")
+    print('Recall: %f' % recall)
+    # f1: tp / (tp + fp + fn)
+    f1 = f1_score(dv_label, model_predict, average="macro")
+    print('F1 score: %f' % f1)
+
+    print('4\tSHAP Bar Of Importance:\n')
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(dval)
+    shap.summary_plot(shap_values, dval, plot_type="bar")
+
+    print('*****\tEVALUATION COMPLETE\t*****')
+    return
+
 
 def main():
-	print("\n\nTeam Losers: Milestone 2\n\n")
-	print("Modifying data for classifiers...\n")
+    print("\n\nTeam Losers: Milestone 2\n\n")
+    print("Modifying data for classifiers...\n")
 
-	data = pd.read_csv(o + "\\..\\data\\cases_train_processed.csv", parse_dates = True)
-	data['sex'] = categorize_column(data['sex'])
-	data['outcome'] = categorize_column(data['outcome'])
-	data['Combined_Key'] = categorize_column(data['Combined_Key'])
+    # data = pd.read_csv(
+    #     o + "\\..\\data\\cases_train_processed.csv", parse_dates=True)
+    data = pd.read_csv(
+        o + "/../data/cases_train_processed.csv", parse_dates=True)
+    data['sex'] = categorize_column(data['sex'])
+    data['outcome'] = categorize_column(data['outcome'])
+    data['Combined_Key'] = categorize_column(data['Combined_Key'])
 
-	#handling date column
-	#removes columns where date_confirmation has a daterange
-	data = data.loc[~data['date_confirmation'].str.contains('-'), :]  
+    # handling date column
+    # removes columns where date_confirmation has a daterange
+    data = data.loc[~data['date_confirmation'].str.contains('-'), :]
 
-	#might need to drop date column if classifiers can't handle it
-	data = data.drop(columns=['province', 'country'])
-	data = data.drop(columns=['date_confirmation'])
+    # might need to drop date column if classifiers can't handle it
+    data = data.drop(columns=['province', 'country'])
+    data = data.drop(columns=['date_confirmation'])
 
-	print("Splitting data into test and validation sets...\n")
+    print("Splitting data into test and validation sets...\n")
 
-	train, val = train_test_split(data, test_size=0.2, random_state=69, shuffle=True)
+    train, val = train_test_split(
+        data, test_size=0.2, random_state=69, shuffle=True)
 
-	print("Building first model...\n")	
-	build_model(train)
+    print("Building training model...\n")
+    build_model(train, val)
 
-	print("Model building completed, Evaluating models...")
+    print("Model building completed, Evaluating models...")
 
+    # -------- Functions for latter parts of the milestone-------------------------
+    evaluate(train, val, xgpath, xg_pass)
 
-	#-------- Functions for latter parts of the milestone-------------------------
-	#evaluate(model,train,val)
+    # show_overfit(model)
 
-	#show_overfit(model)
+    return
 
-	return
 
 if __name__ == '__main__':
     main()
